c6bd6c4981e53d963c87060209945bbc221f445e
Jason Owen <jason.a.owen@gmail.com>
2017-02-17 18:27:52 -0500

    remove default react logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { CardContainer } from './components/card';
 import { GuessContainer } from './components/guess';
@@ -17,10 +16,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <CardContainer />
         <GuessContainer />
       </div>
