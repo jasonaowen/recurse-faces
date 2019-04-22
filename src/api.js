f8d4858fcfdc221fa09ea6d7acea67642d63939f
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-04-22 16:06:19 -0400

    Fixes #29 - Show tenure info (batch name, employment title, relevant dates, etc.) for correct guess

--- src/api.js
+++ src/api.js
@@ -5,12 +5,26 @@ let testPeople = [
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/A_and_B_Larsen_orchids_-_Cattleya_Barbara_Belle_DSCN8696.JPG/176px-A_and_B_Larsen_orchids_-_Cattleya_Barbara_Belle_DSCN8696.JPG",
     last_name: "Belle",
     middle_name: "",
+    stints: [{
+      end_date: "Thu, 20 Aug 2015 00:00:00 GMT",
+      short_name: "",
+      start_date: "Mon, 17 Aug 2015 00:00:00 GMT",
+      stint_type: "residency",
+      title: ""
+    }],
     person_id: 1
   }, {
     first_name: "fIrSt ç",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Cattleya_Portia.jpg/158px-Cattleya_Portia.jpg",
     last_name: "Portia",
     middle_name: "",
+    stints: [{
+      end_date: "",
+      short_name: "",
+      start_date: "Mon, 30 Jul 2018 00:00:00 GMT",
+      stint_type: "employment",
+      title: "Career facilitator"
+    }],
     person_id: 2
   },
   {
@@ -18,6 +32,13 @@ let testPeople = [
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Blc_Nobiles_carnival.jpg/176px-Blc_Nobiles_carnival.jpg",
     last_name: "Carnival",
     middle_name: "",
+    stints: [{
+      end_date: "Thu, 24 Mar 2016 00:00:00 GMT",
+      short_name: "W2'16",
+      start_date: "Mon, 04 Jan 2016 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }],
     person_id: 3
   },
   {
@@ -25,12 +46,38 @@ let testPeople = [
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/A_and_B_Larsen_orchids_-_Cattleya_Miva_Breeze_Alize_930-23.jpg/89px-A_and_B_Larsen_orchids_-_Cattleya_Miva_Breeze_Alize_930-23.jpg",
     last_name: "Alize",
     middle_name: "",
+    stints: [{
+      end_date: "Thu, 17 Dec 2015 00:00:00 GMT",
+      short_name: "F2'15",
+      start_date: "Mon, 28 Sep 2015 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }, {
+      end_date: "Fri, 12 Jan 2018 00:00:00 GMT",
+      short_name: "m1'18",
+      start_date: "Mon, 08 Jan 2018 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }],
     person_id: 4
   }, {
     first_name: "Mârjorie",
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/A_and_B_Larsen_orchids_-_Cattleya_Marjorie_Hausermann_York_812-4.jpg/191px-A_and_B_Larsen_orchids_-_Cattleya_Marjorie_Hausermann_York_812-4.jpg",
     last_name: "York",
     middle_name: "Hausermann",
+    stints: [{
+      end_date: "Thu, 02 May 2013 00:00:00 GMT",
+      short_name: "W'13",
+      start_date: "Mon, 11 Feb 2013 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }, {
+      end_date: "Thu, 10 May 2018 00:00:00 GMT",
+      short_name: "",
+      start_date: "Tue, 30 May 2017 00:00:00 GMT",
+      stint_type: "experimental",
+      title: "Exploring",
+    }],
     person_id: 5
   },
   {
@@ -38,6 +85,25 @@ let testPeople = [
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/A_and_B_Larsen_orchids_-_Cattleya_Little_AngelDSCN3349.JPG/176px-A_and_B_Larsen_orchids_-_Cattleya_Little_AngelDSCN3349.JPG",
     last_name: "Angël",
     middle_name: "",
+    stints: [{
+      end_date: "Fri, 11 Feb 2016 00:00:00 GMT",
+      short_name: "",
+      start_date: "Mon, 08 Feb 2016 00:00:00 GMT",
+      stint_type: "residency",
+      title: ""
+    }, {
+      end_date: "Fri, 26 Feb 2016 00:00:00 GMT",
+      short_name: "",
+      start_date: "Mon, 22 Feb 2016 00:00:00 GMT",
+      stint_type: "residency",
+      title: ""
+    }, {
+      end_date: "Fri, 04 Mar 2016 00:00:00 GMT",
+      short_name: "",
+      start_date: "Mon, 29 Feb 2016 00:00:00 GMT",
+      stint_type: "residency",
+      title: ""
+    }],
     person_id: 6
   },
   {
@@ -45,6 +111,25 @@ let testPeople = [
     image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Cattleya_Hermine.jpg/180px-Cattleya_Hermine.jpg",
     last_name: "Last",
     middle_name: "",
+    stints: [{
+      end_date: "Thu, 05 Nov 2015 00:00:00 GMT",
+      short_name: "F1'15",
+      start_date: "Mon, 17 Aug 2015 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }, {
+      end_date: "Thu, 22 Sep 2016 00:00:00 GMT",
+      short_name: "F1'16",
+      start_date: "Mon, 15 Aug 2016 00:00:00 GMT",
+      stint_type: "retreat",
+      title: ""
+    }, {
+      end_date: "Thu, 10 May 2018 00:00:00 GMT",
+      short_name: "",
+      start_date: "Wed, 26 Jul 2017 00:00:00 GMT",
+      stint_type: "facilitatorship",
+      title: ""
+    }],
     person_id: 7
   }
 ]
--- src/components/guess.js
+++ src/components/guess.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Guess extends Component {
+
   render() {
     const guess = this.props.guess;
     if (guess.get('status') === "correct") {
@@ -9,11 +10,22 @@ class Guess extends Component {
       let firstName = person.getIn(['0','first_name']);
       let lastName = person.getIn(['0','last_name']);
       let personId = person.getIn(['0','person_id']);
+      let stints = person.getIn(['0','stints']);
+
+      let stint_info = new Set(stints.map(s => this.stintToString(s)));
+      stint_info = [...stint_info].join('; ');
+
+      if (stint_info) {
+        stint_info = <span className="stint">{`(${stint_info})`}</span>;
+      } else {
+        stint_info = "";
+      }
+
       return (
           <div className="guess-result correct-guess">
             <p>Correct! Their Full Name
-            is <a href={`https://www.recurse.com/directory/${personId}`}>
-            {firstName} {lastName}</a>
+            is <a href={`https://www.recurse.com/directory/${personId}`}> <br/>
+            {firstName} {lastName}</a> {stint_info}
             </p>
           </div>
       );
@@ -26,6 +38,76 @@ class Guess extends Component {
     } else {
       return null;
     }
+  }
+
+  stintToString(stint) {
+    let stint_info = "";
+
+    if (!stint) {
+      return "";
+    }
+
+    let type = stint.get('stint_type')
+    if (!type) {
+      return "";
+    }
+
+    if (type === 'retreat') {
+      return stint.get('short_name');
+    }
+
+    switch (type) {
+      case 'residency':
+        type = 'Resident';
+        break;
+      case 'employment':
+        type = stint.get('title') || 'Staff';
+        break;
+      case 'experimental':
+        type = 'Experimental Batch';
+        break;
+      case 'research fellowship':
+        type = 'Research Fellow';
+        break;
+      case 'facilitatorship':
+        type = 'Facilitator';
+        break;
+      default:
+        type = type.charAt(0).toUpperCase() + type.slice(1);
+    }
+
+    let dates = this.dateRangeToString(stint.get('start_date'),
+      stint.get('end_date'));
+
+    // Using non-breaking space char to keep stint together on a single line
+    stint_info = (type + dates).replace(/\s+/g, "\u00A0")
+
+    return stint_info;
+  }
+
+  dateRangeToString(start_date, end_date) {
+    let date = "";
+    const start = this.dateToString(start_date);
+
+    // Schema requires start date but not end date
+    if (end_date) {
+      date = ", " + start;
+      const end = this.dateToString(end_date);
+
+      if (start !== end) {
+        date += " - " + end;
+      }
+    } else {
+      date = " since " + start;
+    }
+
+    return date;
+  }
+
+  dateToString(date) {
+    // Input format: "Day, DD Mon YYYY", e.g. "Tue, 23 Apr 2019 00:00:00 GMT"
+    // Output format: "Mon 'YY", e.g. "Apr '19"
+    return date.substring(8, 11) + " '" + date.substring(14, 16);
   }
 }
 
